d8995b059ba3694c217e1fbfb08dfb1fe064c8c2
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-10-29 19:41:38 +0300

    Setup and initialization

---
+++ HashMap.js
@@ -0,0 +1,9 @@
+class HashMap {
+    constructor(capacity , loadFactor) {
+        this.capacity = 16;
+        this.loadFactor = 0.75;
+        this.buckets = [null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,];
+        this.length =0;
+
+    }
+}
